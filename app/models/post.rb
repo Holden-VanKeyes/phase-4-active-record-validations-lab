9ffa4439daaf26b21a4743d632482461493d6c48
Abram Shook <ajshook77@gmail.com>
2022-06-30 17:45:36 -0500

    commit tree 62858c14f494943d648605456cf92a1e16027ef9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :click_bait
+    
+    # def click_bait
+    #   allowed = ["Won't Believe", "Secret"]
+    #  if title.match?(allowed)
+
+    #  else
+    #     errors.add(:title, 'not allowed')
+    #  end
+    # end
+
+    def click_bait
+      unless title.present? &&
+        title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+
+       
+        errors.add(:title, 'not allowed')
+     end 
+    end
 end
